eddae657216f31816f97af752ff7585bcb11c331
Francesco Galasso <francesco.galasso7@studio.unibo.it>
2024-04-27 12:37:24 +0200

    Modified main with a few tries

--- ionization/main.py
+++ ionization/main.py
@@ -1,8 +1,21 @@
-import numpy as np
 import ionization_fit as ion
 import ionization_data as id
 
 id.plot_ionization_data()
 
-# ionization fit with threshold at 25 V
-ion.ion_fit_N()
+# results = []
+# 
+# for i in range(5):
+#    for j in range(i + 1, 9):
+#        print(f'\nV_min = {15+5*i} V, V_max = {15+5*j} V')
+#        r = ion.ion_fit(15 + 5 * i, 15 + 5 * j, 'CO2')
+#        results.append(-r[0, 0] / r[0, 1])
+# 
+# print(results)
+
+
+ion.ion_fit(22, 36, 'N')
+ion.ion_fit(15, 55, 'N')
+ion.ion_fit(20, 38, 'Ar')
+ion.ion_fit(18, 40, 'Ar')
+ion.ion_fit(15, 55, 'Ar')
